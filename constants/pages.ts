08b608dfab470a72edd32008a47c7862868a125a
eunchae <kec0130@gmail.com>
2023-12-25 23:48:59 +0900

    Add tags

--- constants/pages.ts
+++ constants/pages.ts
@@ -3,6 +3,27 @@ export const navLinks = [
   { name: 'Blog', path: '/blog' },
 ] as const;
 
-export const tags = ['React', 'Next.js', 'Python', 'FastAPI'] as const;
-
 export const authors = ['EunchaeKo', 'mingke'] as const;
+
+export const tags = [
+  'Python',
+  'FastAPI',
+  'SQLAIchemy',
+  'Pytest',
+  'GithubActions',
+  'S3',
+  'AWS',
+  'CloudWatch',
+  'ECS',
+  'Swagger',
+  'Redis',
+  'Aiohttp',
+  'Pydantic',
+  'Gunicorn',
+  'Boto3',
+  'Asyncio',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+] as const;
