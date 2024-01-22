638ccf216160587213222c81c587e5cc555e21e9
eunchae <kec0130@gmail.com>
2024-01-22 17:39:37 +0900

    Add year and tech stack

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -20,7 +20,12 @@ const Page = ({ params: { slug } }: Props) => {
   return (
     <article className="prose prose-slate mx-auto max-w-[730px] py-6 lg:prose-lg md:py-10">
       <header>
-        <h1 className="!mb-0">{project.title}</h1>
+        <div className="flex items-center">
+          <h1 className="!mb-0">{project.title}</h1>
+          <span className="ml-3 rounded-full bg-slate-200/60 px-2 py-0.5 text-sm md:ml-4 md:px-3 md:py-1">
+            {project.year}
+          </span>
+        </div>
         <p className="lead !my-5 md:!my-6">{project.description}</p>
       </header>
       <hr className="!my-0" />
