c2b31734ae622d7cdf8496b9505d466549086253
JungMinChae <60789129+Jungminchae@users.noreply.github.com>
2024-01-26 21:59:47 +0900

    add mingke profile
    * add mingke about
    
    * Update about eko section
    
    * Update profile

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -10,6 +10,9 @@ import GithubIcon from './github.svg';
 import ProfileEkoIcon1 from './profile-eko-1.svg';
 import ProfileEkoIcon2 from './profile-eko-2.svg';
 import ProfileEkoIcon3 from './profile-eko-3.svg';
+import ProfileMingkeIcon1 from './profile-mingke-1.svg';
+import ProfileMingkeIcon2 from './profile-mingke-2.svg';
+import ProfileMingkeIcon3 from './profile-mingke-3.svg';
 
 export {
   Logo,
@@ -23,4 +26,7 @@ export {
   ProfileEkoIcon1,
   ProfileEkoIcon2,
   ProfileEkoIcon3,
+  ProfileMingkeIcon1,
+  ProfileMingkeIcon2,
+  ProfileMingkeIcon3,
 };
