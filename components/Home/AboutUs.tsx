a1720f39f445d855106fcfbffe69c7ea3281b972
eunchae <kec0130@gmail.com>
2024-03-05 12:29:33 +0900

    Fix icon size

--- components/Home/AboutUs.tsx
+++ components/Home/AboutUs.tsx
@@ -20,15 +20,15 @@ const profiles: ProfileCardProps[] = [
     iconColor: 'fill-pink-500',
     descriptions: [
       {
-        icon: <ProfileEkoIcon1 />,
+        icon: <ProfileEkoIcon1 className="h-8 w-8" />,
         text: 'Next.js와 TypeScript를 사용하여 빠르고 안정적인 웹사이트를 개발합니다.',
       },
       {
-        icon: <ProfileEkoIcon2 />,
+        icon: <ProfileEkoIcon2 className="h-8 w-8" />,
         text: '편리하고 직관적인 UI/UX를 설계하기 위해 사용자 관점에서 고민하고, 모든 기기에서 원활하게 작동하는 반응형 웹사이트를 개발합니다.',
       },
       {
-        icon: <ProfileEkoIcon3 />,
+        icon: <ProfileEkoIcon3 className="h-8 w-8" />,
         text: '개발뿐만 아니라 기획, 디자인, SEO 등 서비스 출시에 필요한 모든 과정을 직접 수행할 수 있습니다.',
       },
     ],
@@ -42,15 +42,15 @@ const profiles: ProfileCardProps[] = [
     iconColor: 'fill-indigo-500',
     descriptions: [
       {
-        icon: <ProfileMingkeIcon1 />,
+        icon: <ProfileMingkeIcon1 className="h-8 w-8" />,
         text: 'FastAPI 또는 Django를 사용하여 안정적인 서버를 구축하는 Python 개발자입니다.',
       },
       {
-        icon: <ProfileMingkeIcon2 />,
+        icon: <ProfileMingkeIcon2 className="h-8 w-8" />,
         text: '컨테이너 기술과 AWS를 사용하여 서버를 배포하고, CI/CD를 구축하여 개발 생산성을 높일 수 있습니다.',
       },
       {
-        icon: <ProfileMingkeIcon3 />,
+        icon: <ProfileMingkeIcon3 className="h-8 w-8" />,
         text: '새로운 기술을 배우고 지식을 공유하는 것을 좋아합니다. 최근 관심사는 프롬프트 엔지니어링입니다.',
       },
     ],
