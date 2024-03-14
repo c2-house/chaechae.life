c679e79c464b6a1942b4b4bbabca24fa1f6f061b
eunchae <kec0130@gmail.com>
2024-03-14 16:28:41 +0900

    Revert "Fix project container"
    This reverts commit 2dc84cf7d029ef532c50bcd1623decdf52165a63.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
           <ProjectListItem key={project.slug} project={project} />
         ))}
 
-        <li className="flex aspect-square h-auto w-full max-w-[470px] flex-col items-center justify-center rounded-xl border-2 border-gray-100">
+        <li className="flex aspect-square max-w-[470px] flex-col items-center justify-center rounded-xl border-2 border-gray-100">
           <WebsiteIcon className="mb-5 h-36 w-36 lg:mb-7 lg:h-44 lg:w-44" />
           <p className="text-xl text-slate-500 lg:text-2xl">
             <span className="mr-1">오늘도 개발 중</span>
@@ -29,10 +29,10 @@ const Page = () => {
           </p>
         </li>
 
-        <li className="flex h-auto w-full max-w-[470px]">
+        <li className="flex max-w-[470px]">
           <ResponsiveAds />
         </li>
-        <li className="flex h-auto w-full max-w-[470px]">
+        <li className="flex max-w-[470px]">
           <ResponsiveAds />
         </li>
       </ul>
--- components/Projects/ProjectListItem.tsx
+++ components/Projects/ProjectListItem.tsx
@@ -6,7 +6,7 @@ const ProjectListItem = ({ project }: { project: Project }) => {
   return (
     <li>
       <Link href={`/projects/${project.slug}`}>
-        <article className="group mx-auto aspect-square h-auto w-full max-w-[470px] overflow-hidden rounded-xl bg-gray-100 p-5 lg:p-8">
+        <article className="group mx-auto aspect-square max-w-[470px] overflow-hidden rounded-xl bg-gray-100 p-5 lg:p-8">
           <header>
             <h2 className="text-2xl font-semibold lg:text-3xl">{project.title}</h2>
             <p className="my-1 text-slate-500 lg:my-2 lg:text-lg">{project.description}</p>
