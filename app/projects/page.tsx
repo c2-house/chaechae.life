44f084456153b88fde9c9b6757b77b654df3d365
eunchae <kec0130@gmail.com>
2024-03-14 16:29:18 +0900

    Revert "Fix ads style"
    This reverts commit 95c0635117e8323245d0a1c3bede30239a41c99e.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,7 +12,6 @@ const Page = () => {
         {projects.map((project) => (
           <ProjectListItem key={project.slug} project={project} />
         ))}
-
         <li className="flex aspect-square max-w-[470px] flex-col items-center justify-center rounded-xl border-2 border-gray-100">
           <WebsiteIcon className="mb-5 h-36 w-36 lg:mb-7 lg:h-44 lg:w-44" />
           <p className="text-xl text-slate-500 lg:text-2xl">
@@ -28,14 +27,8 @@ const Page = () => {
             ))}
           </p>
         </li>
-
-        <li className="flex max-w-[470px]">
-          <ResponsiveAds />
-        </li>
-        <li className="flex max-w-[470px]">
-          <ResponsiveAds />
-        </li>
       </ul>
+      <ResponsiveAds />
     </main>
   );
 };
