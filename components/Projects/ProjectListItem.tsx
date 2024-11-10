95be8c6fc7ce43728eb32c1be6a6f7ecfe5e397d
Eunchae Ko <77032760+kec0130@users.noreply.github.com>
2024-11-11 00:46:23 +0900

    Fix project grid (#127)

--- components/Projects/ProjectListItem.tsx
+++ components/Projects/ProjectListItem.tsx
@@ -4,9 +4,9 @@ import { Project } from 'contentlayer/generated';
 
 const ProjectListItem = ({ project }: { project: Project }) => {
   return (
-    <li>
+    <li className="group mx-auto flex aspect-square w-full max-w-[470px] flex-col overflow-hidden rounded-xl bg-gray-100">
       <Link href={`/projects/${project.name}`}>
-        <article className="group mx-auto flex aspect-square max-w-[470px] flex-col overflow-hidden rounded-xl bg-gray-100 p-5 lg:p-8">
+        <article className="p-5 lg:p-8">
           <header>
             <h2 className="text-2xl font-semibold lg:text-3xl">{project.title}</h2>
             <p className="my-1 text-slate-500 lg:my-2 lg:text-lg">{project.description}</p>
