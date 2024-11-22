18e88ded2d35e869c4c514eb9a2dff9bbb8ecbc3
EunchaeKo <kec0130@gmail.com>
2024-11-22 23:26:45 +0900

    Add mdx components

---
+++ components/Mdx/Components.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+export const CustomLink = (props: any) => {
+  if (props.href.startsWith('/')) {
+    return <Link href={props.href}>{props.children}</Link>;
+  }
+
+  if (props.href.startsWith('http')) {
+    return <a target="_blank" rel="noopener noreferrer" className="text-indigo-600" {...props} />;
+  }
+
+  return <a {...props} />;
+};
+
+export const Callout = ({
+  emoji = '💡',
+  children,
+}: {
+  emoji?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="flex rounded bg-slate-100 p-4 text-slate-700">
+      <div className="mr-2 text-lg">{emoji}</div>
+      <div className="callout">{children}</div>
+    </div>
+  );
+};
+
+export const Flex = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex flex-wrap justify-center gap-8">{children}</div>;
+};
